7d65994a608c540993a7f025cc8149a78579bded
Leigh Michael Forrest <leighmforrest@gmail.com>
2025-07-07 13:41:34 -0400

    click on main enabled

---
+++ src/scripts/index.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+  /** DOM ELEMENTS THAT MATTER */
+  const hamburger = document.querySelector(".hamburger");
+  const darkModeButton = document.querySelector(".darkMode");
+  const navMenu = document.querySelector(".navMenu");
+  const main = document.querySelector("main");
+
+  /** Event Listeners */
+  hamburger?.addEventListener("click", () => {
+    navMenu?.classList.toggle("hidden");
+  });
+
+  main?.addEventListener("click", ()=> {
+    if (!navMenu.classList.contains("hidden")) navMenu.classList.add("hidden")
+  })
+
+  darkModeButton?.addEventListener("click", () => {
+    document.querySelector("body")?.classList.toggle("dark");
+  });
+});
--- src/scripts/menu.js
+++
@@ -1,7 +0,0 @@
-document.querySelector(".hamburger")?.addEventListener("click", () => {
-    document.querySelector(".navMenu").classList.toggle("hidden")
-})
-
-document.querySelector(".darkMode").addEventListener("click", ()=> {
-    document.querySelector("body")?.classList.toggle("dark")
-})
