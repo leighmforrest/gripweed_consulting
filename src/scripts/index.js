c86085c7adfdd7c30a31d681d007533c9ff552cf
Leigh Michael Forrest <leighmforrest@gmail.com>
2025-07-10 19:12:23 -0400

    contact form panel added

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,20 +2,16 @@ document.addEventListener("DOMContentLoaded", () => {
 
   /** DOM ELEMENTS THAT MATTER */
   const hamburger = document.querySelector(".hamburger");
-  const darkModeButton = document.querySelector(".darkMode");
   const navMenu = document.querySelector(".navMenu");
   const main = document.querySelector("main");
 
   /** Event Listeners */
   hamburger?.addEventListener("click", () => {
     navMenu?.classList.toggle("hidden");
+    console.log(navMenu)
   });
 
   main?.addEventListener("click", ()=> {
     if (!navMenu.classList.contains("hidden")) navMenu.classList.add("hidden")
   })
-
-  // darkModeButton?.addEventListener("click", () => {
-  //   document.querySelector("body")?.classList.toggle("dark");
-  // });
 });
